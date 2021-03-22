497f639ca8285a37c75fba3d0e03c937f4478e7a
Olga <73503512+matyldv@users.noreply.github.com>
2021-03-22 22:18:03 +0100

    Update szpital.sql

--- szpital.sql
+++ szpital.sql
@@ -35,35 +35,59 @@ create table Sprzety(
 	numer_seryjny varchar2(20) primary key,
 	nazwa varchar2(50) not null,
 	producent varchar2(50) not null,
-	rok_produkcji date not null,
-	nazwa_oddzialu varchar2(50) references Oddzialy(nazwa) not null);
+	rok_produkcji number not null,
+	nazwa_oddzialu varchar2(50) not null,
+	constraint fk_zamkniecie_oddzialu
+	foreign key (nazwa_oddzialu)
+	references Oddzialy(nazwa)
+	on delete cascade);
 	
 create table Sale(
 	numer number(4) primary key,
 	pietro number(2) not null,
-	oddzial varchar2(50) references Oddzialy(nazwa) not null);
+	oddzial varchar2(50),
+	constraint fk_usuniecie_oddzialu
+	foreign key (oddzial)
+	references Oddzialy(nazwa)
+	on delete cascade);
 
 create table Badania(
 	nazwa varchar2(100) primary key,
-	cena number(4,2) not null,
+	cena number(4) not null,
 	czas_oczekiwania number(2) not null);
 	
 create table BadaniaPacjentow(
 	id_badania number(6) primary key,
-	nazwa_badania varchar2(100) references Badania(nazwa) not null,
+	nazwa_badania varchar2(100) not null,
 	pacjent varchar2(20) references Pacjenci(pesel) not null,
 	lekarz number(6) references Lekarze(id_lekarza) not null,
-	data_badania date not null);
+	data_badania date not null,
+	constraint fk_usuniecie_badania
+	foreign key (nazwa_badania)
+	references badania(nazwa)
+	on delete cascade,
+	constraint fk_usuniecie_pacjenta_badania
+	foreign key (pacjent)
+	references Pacjenci(pesel)
+	on delete cascade);
 	
 create table Wyniki(
 	id_wyniku number(6) primary key,
-	id_badania references BadaniaPacjentow(id_badania) not null,
+	id_badania not null,
 	wynik_badania varchar2(100) not null,
-	zalecenia varchar2(100));
+	zalecenia varchar2(100),
+    constraint fk_usuniecie_wyniku
+	foreign key (id_badania)
+	references BadaniaPacjentow(id_badania)
+	on delete cascade);
 
 create table Przyjecia(
 	data_przyjecia date,
 	lekarz number(6) references Lekarze(id_lekarza),
-	pacjent varchar2(20) references Pacjenci(pesel),
+	pacjent varchar2(20) ,
 	primary key (data_przyjecia, lekarz, pacjent),
-	nr_sali number(4) references Sale(numer) not null);
+	nr_sali number(4) references Sale(numer) not null,
+	constraint fk_usuniecie_pacjenta
+	foreign key (pacjent)
+	references Pacjenci(pesel)
+	on delete cascade);
